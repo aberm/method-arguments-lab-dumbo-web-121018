79907e1851629c29089d6ff65ed8697786935328
Menachem Aber <abermendel@gmail.com>
2018-11-22 01:36:27 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,4 +2,5 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language(name, lang)}.to output("Hi, my name is Dan and I am learning to program in Ember.js.\n")
+def introduction_with_language(name, lang)
+  puts "Hi, my name is #{name} and I am learning to program in #{lang}.")
